f8cdab9687a9cef2f9d44ca4bdf77d88db771a84
Etienne_MR <61475533+EtienneMR@users.noreply.github.com>
2024-08-10 19:36:51 +0200

    Roll back to working fix

--- server/api/files/index.delete.ts
+++ server/api/files/index.delete.ts
@@ -1,10 +1,11 @@
 export default eventHandler(async (event) => {
     await requireUserSession(event)
 
+    // @ts-expect-error - req.body is not defined in the type definitions
     // workaround to prevent cloudflare "The script will never generate a response." error.
-    const pathnames = await readRawBody(event)
+    const pathnames: string = event.node.req.body ?? await readRawBody(event)
 
-    await hubBlob().del(JSON.parse(pathnames!))
+    await hubBlob().del(JSON.parse(pathnames))
 
     return sendNoContent(event)
 })
