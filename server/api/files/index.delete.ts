da283bff9278b8abaee03e793ed1dc10e783155a
Etienne_MR <61475533+EtienneMR@users.noreply.github.com>
2024-08-10 15:05:53 +0200

    Fix "runtimeConfig is not defined" error

--- server/api/files/index.delete.ts
+++ server/api/files/index.delete.ts
@@ -3,10 +3,9 @@ export default eventHandler(async (event) => {
 
     const pathnames = await readValidatedBody(event, (data) => Array.isArray(data) && data.every(path => typeof path == "string")) as unknown as string[]
 
-    try {
+    if (import.meta.dev) {
         await hubBlob().del(pathnames)
-    } catch (error) {
-        console.error(error)
+    } else {
         await Promise.all(pathnames.map(pathname => hubBlob().del(pathname)))
     }
 
